9900df0694c65c41ab209e073e654eb0a56f1ab9
Ethan Nolan <ethan.nolan@my.jcu.edu.au>
2022-10-09 21:56:21 +1000

    Add Prac 04 list exercises update (Usernames 2)

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -12,7 +12,7 @@ def main():
     usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
                  'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
                  'bob']
-    userInput = input("Enter Username")
+    userInput = input("Enter Username: ")
     if (userInput in usernames):
         print("Access Granteded")
     else:
