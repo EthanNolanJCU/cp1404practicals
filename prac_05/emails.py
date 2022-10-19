2fc974a08f05fc1387df0e573a5fc1bd260a9790
Ethan Nolan <ethan.nolan@my.jcu.edu.au>
2022-10-19 22:41:18 +1000

    Add prac 5 emails Update

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,25 +1,31 @@
-email_to_name = {}
+def main():
+    email_to_name = {}
 
-while True:
-    user_email = input("Email: ")
-    if user_email == "":
-        break
+    while True:
+        user_email = input("Email: ")
+        if user_email == "":
+            break
 
-    name = " ".join(user_email.split("@")[0].split(".")).title()
+        name = get_name_func(user_email)
 
-    yes_list = ["yes", "ye", "y", "1", ""]
-    no_list = ["no", "n"]
-    user_name_correct = input(f"Is your name {name}? (Y/N)").lower()
+        yes_list = ["yes", "ye", "y", "1", ""]
+        no_list = ["no", "n"]
+        user_name_correct = input(f"Is your name {name}? (Y/N)").lower()
 
-    if (user_name_correct in yes_list):
-        pass
-    elif user_name_correct in no_list:
-        name = input("Enter name: ")
-    else:
-        print("Bad Input")
-        print(user_name_correct)
-        break
-    email_to_name[user_email] = name
+        if user_name_correct in no_list:
+            name = input("Enter name: ")
+        elif user_name_correct not in yes_list:
+            print("Bad Input: ")
+            print(user_name_correct + " - Not Valid")
+            break
+        email_to_name[user_email] = name
 
-for email, name in email_to_name.items():
-    print(f"{name} ({email})")
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+def get_name_func(email):
+    return " ".join(email.split("@")[0].split(".")).title()
+
+
+main()
