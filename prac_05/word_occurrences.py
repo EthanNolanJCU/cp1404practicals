f990c848fe1707b022b78c00887f8885f99acfb3
Ethan Nolan <ethan.nolan@my.jcu.edu.au>
2022-10-19 22:35:48 +1000

    Add prac 5 word occurrences Update

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,25 +1,12 @@
-"""
-Word Occurences
-Estimate: 30mins
-Actual: 8 mins
-"""
-from operator import itemgetter
-
 word_to_count = {}
 
-line = input("Enter line: ").lower()
-words = line.split()
 
+words = input("Text: ").split()
 for word in words:
-    if word in word_to_count.keys():
+    try:
         word_to_count[word] += 1
-    else:
+    except KeyError:
         word_to_count[word] = 1
-
-max_word_len = max(len(word) for word in word_to_count.keys())
-max_count_len = max(len(str(count)) for count in word_to_count.values())
-
-word_to_count = dict(sorted(word_to_count.items()))
-
-for word, count in word_to_count.items():
-    print(f"{word:<{max_word_len}} : {count:>{max_count_len}}")
+max_length = max(len(word) for word in word_to_count.keys())
+for word in word_to_count.keys():
+    print(f"{word:{max_length}} : {word_to_count[word]}")
