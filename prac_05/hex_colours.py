2ceb04f6cf0bf061e3c21f53f072854f17b37622
Ethan Nolan <ethan.nolan@my.jcu.edu.au>
2022-10-19 22:34:38 +1000

    Add prac 5 hex colours Update

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,13 +3,10 @@ COLOUR_TO_HEX = {"absolute zero": "#0048ba", "acid green": "#b0bf1a", "aliceblue
                "antiquewhite": "#faebd7", "antiquewhite1": "#ffefdb", "antiquewhite2": "#eedfcc",
                "antiquewhite3": "#cdc0b0", "antiquewhite4": "#8b8378", "apricot": "#fbceb1", "aqua": "#00ffff"}
 
-
-while True:
-    colour_input = input("Enter Colour Name: ").lower()
-    if (colour_input == ""):
-        print("End.")
-        break
+colour_input = input("Enter Colour Name: ").lower()
+while colour_input != "":
     try:
         print(f"{colour_input}:{COLOUR_TO_HEX[colour_input]}")
     except:
         print("Invalid Colour.")
+    colour_input = input("Enter Colour Name: ").lower()
