396a675ecd27943349a21aed115dbac82dad8c70
Ethan Nolan <ethan.nolan@my.jcu.edu.au>
2022-11-01 14:18:10 +1000

    Add Prac 06 corrected names of files

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,18 +1,9 @@
 from prac_06.guitar import Guitar
-print("My Guitars!")
-my_guitars = []
-name = input("Name: ")
-while name != '':
-    year = int(input("Year: "))
-    cost = float(input("Price: ").strip('$'))
-    my_guitars.append(Guitar(name, year, cost))
-    print(f"{my_guitars[-1]} Added.")
-    name = input("Name: ")
 
-name_index = max(len(index.name) for index in my_guitars)
-price_index = max(len(str(index.cost)) for index in my_guitars)
+guitar1 = Guitar("Gibson", 1922, 12.50)
+guitar2 = Guitar("other", 2013, 3000)
 
-index = 1
-for entry in my_guitars:
-    print(f"Guitar {index}: {entry.name:>{name_index}} ({entry.year:>4}), worth $ {entry.cost:>{price_index},.2f}", '(vintage)' if entry.is_vintage() else '')
-
+print(f"expected 100, got {guitar1.get_age()}")
+print(f"expected 9, got {guitar2.get_age()}")
+print(f"expected True, got {guitar1.is_vintage()}")
+print(f"expected False, got {guitar2.is_vintage()}")
