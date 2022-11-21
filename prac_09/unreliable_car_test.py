0c6f6c3aeb50ff04be98ac0d492f4df2206a5627
Ethan Nolan <ethan.nolan@my.jcu.edu.au>
2022-11-21 12:04:26 +1000

    Add Prac 09 unreliable car task

---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,10 @@
+from prac_09.unreliable_car import UnreliableCar
+
+my_car = UnreliableCar("Name", 100, 50)
+
+my_car.drive(10)
+print(my_car)
+my_car.drive(10)
+print(my_car)
+my_car.drive(10)
+print(my_car)
